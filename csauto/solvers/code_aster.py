d108c4b7675aeb0780c5b01ee3c9e5f8d46286ea
Maxime Gueguin <maxime.gueguin@simvia.tech>
2026-07-31 10:21:30 +0200

    [dev] full working version docker - singularity code_aster

--- csauto/solvers/code_aster.py
+++ csauto/solvers/code_aster.py
@@ -22,7 +22,6 @@ from ..registry import STATUS_DONE, STATUS_FAILED
 from .base import CompareKind, SolverAdapterBase
 
 CODE_ASTER_EXPORT_EXTENSION = "export"
-CODE_ASTER_LOG_EXTENSION = "mess"
 
 class CodeAsterAdapter(SolverAdapterBase):
     name: ClassVar[str] = "code_aster"
@@ -30,8 +29,9 @@ class CodeAsterAdapter(SolverAdapterBase):
     container_bin_name: ClassVar[str] = ""
     container_root: ClassVar[str] = "/home/user"
     default_docker_image: ClassVar[str] = "simvia/code_aster:17.4.0"
-    results_dirname: ClassVar[str] = "RESULTS"
-    shared_dir_names: ClassVar[tuple[str, ...]] = ("MESH", )
+    results_dirname: ClassVar[str] = "RESU"
+    logs_dirname: ClassVar[str] = "LOGS"
+    shared_dir_names: ClassVar[tuple[str, ...]] = ("MESH", "RESU", )
     export_file: ClassVar[str] = "study.export"
 
     def build_run_command(
@@ -45,62 +45,92 @@ class CodeAsterAdapter(SolverAdapterBase):
         run_args: Sequence[str] | None = None,
         cleanenv: bool = True,
         env_vars: Mapping[str, str] | None = None,
+        tmp_name: str = "TMP"
     ) -> list[str]:
         """Build the docker command to launch a case."""
         runs_root = case_dir.parent.resolve()
         container_root = self.container_root
         container_case = f"{container_root}/{case_dir.name}"
-        filename = str(self.export_file).split('/')[-1]
+        exportfile = str(self.export_file).split('/')[-1]
+        host_tmpdir = f"{runs_root}/{case_dir.name}/{tmp_name}"
+        solverlogpath = f"{self.results_dirname}/{self.logs_dirname}/run_solver.log"
 
-        links = []
+        with open(f"{runs_root}/{case_dir.name}/{exportfile}", "a") as f:
+            f.write(f"F mess {solverlogpath} R 6\n")
+
+        links = [ f"{runs_root}:{container_root}" ]
         for i, shared_data in enumerate(shared_dir_symlink_mounts(runs_root, self.shared_dir_names)):
             readonly = shared_data[1]
             linked = f"{shared_data[0]}:{container_case}/{list(self.shared_dir_names)[i]}"
             links.append(f"{linked}:ro" if readonly else f"{linked}")
 
+        #TODO: Pour relier le fichier de message temporaire
+        #solverlogfile = Path(f"{runs_root}/{case_dir.name}/{solverlogpath}")
+        #links.append(f"{runs_root}/{case_dir.name}/{solverlogpath}:{container_case}/{tmp_name}/proc.0/fort.6")
+
+        #TODO : Ajouter la suppression de tous les dossiers partagé sauf celui de resultats
+        rmdir = [ f"{host_tmpdir}" ]
+
         if selection.runtime == RUNTIME_DOCKER:
-            cmd: list[str] = ["nohup", "docker", "run", "-v", f"{runs_root}:{container_root}"]
-            for link in links:
-                cmd.extend(["-v", link])
-            for key, value in sorted((env_vars or {}).items()):
-                cmd.extend(["-e", f"{key}={value}"])
-            cmd.extend(["--label", f"csauto.case_id={case_dir.name}"])
-            if cidfile:
-                cmd.extend(["--cidfile", str(cidfile)])
-            cmd.extend(["-w", container_case, selection.docker_image])
-            launch_cmd = "source /opt/activate.sh &&"
-            launch_cmd += f" run_aster {filename}"
-            # launch_cmd += f" --wrkdir {container_case}/tmp/ && rm -r {container_case}/tmp/"
-            cmd.extend(["bash", "-i", "-c", launch_cmd])
-        elif selection.runtime == RUNTIME_SINGULARITY:
+            add_cid = f"--cidfile {str(cidfile)}" if cidfile else ""
+
+            bind_links = " ".join(f"-v {link}" for link in links)
+            removed_dirs = " ".join(f"rm -rf {dire}" for dire in rmdir)
+            
             cmd = [
                 "nohup",
                 "bash",
                 "-c",
-                f"mkdir -p ~/apptainer_tmp && "
-                f"export APPTAINER_TMPDIR=~/apptainer_tmp && "
-                f"export TMPDIR=~/apptainer_tmp && "
+                f"docker run "
+                f"{bind_links} "
+                f"-w {container_case} "
+                f"--label csauto.case_id={case_dir.name} "
+                f"{add_cid} "
+                f"{selection.docker_image} "
+                f"bash -c 'source /opt/activate.sh && run_aster {exportfile} --wrkdir {container_case}/{tmp_name}/'; "
+                f"{removed_dirs}"
+            ]
+        elif selection.runtime == RUNTIME_SINGULARITY:
+            host_apptainer = f"~/apptainer_tmp_{case_dir.name}"
+            rmdir.append(f"{host_apptainer}")
+
+            bind_links = " ".join(f"--bind {link}" for link in links)
+            removed_dirs = " ".join(f"rm -rf {dire}" for dire in rmdir)
+            
+            cmd = [
+                "nohup",
+                "bash",
+                "-c",
+                f"mkdir -p {host_apptainer} && "
+                f"export APPTAINER_TMPDIR={host_apptainer} && "
                 f"{selection.singularity_bin} exec "
-                f"--bind {runs_root}:{container_root} "
-                f"--bind {links[0]} "
+                f"{bind_links} "
                 f"--pwd {container_case} "
                 f"{selection.singularity_image} "
-                f"bash -c 'source /opt/activate.sh && run_aster {filename}' && "
-                f"rm -rf ~/apptainer_tmp && "
-                f"unset APPTAINER_TMPDIR TMPDIR"       
+                f"bash -c 'source /opt/activate.sh && run_aster {exportfile} --wrkdir {container_case}/{tmp_name}/'; "
+                f"{removed_dirs}"
             ]
         elif selection.runtime == RUNTIME_NATIVE:
             #TODO
-            raise NotImplementedError(f"{RUNTIME_NATIVE} must be implemented for code_aster.")
+            raise NotImplementedError(
+                f"{RUNTIME_NATIVE} must be implemented for code_aster."
+            )
         else:
-            raise NotImplementedError(f"{selection.runtime} not in the development roadmap of code_aster.")
+            if selection.runtime in ['cave', 'salome_meca']:
+                #TODO
+                raise NotImplementedError(
+                    f"{selection.runtime} could be in the roadmap of code_aster. Please contact support."
+                )
+            else:
+                raise NotImplementedError(
+                    f"{selection.runtime} not in the development roadmap of code_aster."
+                )
 
         return cmd
 
     def run_argv(
             self, case_path: str | Path, nprocs: int, nt: int, run_args: Sequence[str] | None = None
         ) -> list[str]:
-        #return [str(self.export_file).split('/')[-1]]
         return [""]
 
     def find_setup_file(self, template_dir: Path) -> Path:
@@ -125,7 +155,7 @@ class CodeAsterAdapter(SolverAdapterBase):
             re.compile(r"DIAGNOSTIC JOB : NO_TEST_RESU", re.IGNORECASE),
             re.compile(r"DIAGNOSTIC JOB : NOOK_TEST_RESU", re.IGNORECASE)
         ]
-        files = list(Path(case_dir).glob(f"*.{CODE_ASTER_LOG_EXTENSION}"))
+        files = list(Path(f"{case_dir}/{self.results_dirname}/{self.logs_dirname}/").glob(f"run_solver.log"))
         if len(files) == 0:
             return None
         elif len(files) == 1:
